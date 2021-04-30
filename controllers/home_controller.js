96747107abd5d27c7a2f071100ce6b6d0218662c
Ashish Raghuvanshi <thetypo36@gmail.com>
2021-04-30 22:27:47 +0530

    add views to home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,6 @@
 module.exports.home = function (req, res) {
-    res.end("<h1>express is up for codeial</h1>");
+    // res.end("<h1>express is up for codeial</h1>");
+    res.render('home', {
+        title: "home"
+    });
 }
