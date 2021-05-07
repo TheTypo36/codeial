edbf91d7d0f0483a9694899716bf1d54cda132cc
Ashish Raghuvanshi <thetypo36@gmail.com>
2021-05-07 09:05:26 +0530

    add sign form action and routes to create a user

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,4 @@
+const User = require('../models/user')
 module.exports.profile = function (req, res) {
     res.render('user_profile', {
         title: "User_profile"
@@ -13,11 +14,32 @@ module.exports.signUp = function (req, res) {
 module.exports.signIn = function (req, res) {
     res.render('user_signin.ejs', {
         title: "codeail | Sign In"
-    })
+    });
 }
 
 module.exports.create = function (req, res) {
+    if (req.body.password != req.body.confirmPassword) {
+        return res.redirect('back');
+    }
+    User.findOne({ email: req.body.email }, function (err, user) {
+        if (err) {
+            console.log('error in find the error: ', err);
+            return;
+        }
+        if (!user) {
+            User.create(req.body, function (err, user) {
+                if (err) {
+                    console.log('error in creating user while sign in: ', err);
+                    return;
+                }
+                return res.redirect('/user/sign-in');
+            });
+        } else {
+            return res.redirect('back');
+        }
 
+
+    });
 }
 module.exports.createSession = function (req, res) {
     //todo later
