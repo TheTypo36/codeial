471a0c8403617e56c69f8ed6b7a8e55f145fc715
Ashish Raghuvanshi <thetypo36@gmail.com>
2021-05-16 13:43:41 +0530

    create a scheme for post and routes and controller for it

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,5 +1,6 @@
 module.exports.ImgPost = function (req, res) {
-    res.end('<h1>This is img of 1 post</h1>');
+    console.log(req.body);
+    res.end('<h1>req.body.content </h1>');
 }
 module.exports.videoPost = function (req, res) {
     res.end('<h1>This is video of 1 post</h1>');
--- routes/post.js
+++ routes/post.js
@@ -3,6 +3,6 @@ const express = require('express');
 const router = express.Router();
 
 const postController = require('../controllers/post_controller');
-router.get('/img', postController.ImgPost);
+router.post('/img', postController.ImgPost);
 router.get('/video', postController.videoPost);
 module.exports = router;
