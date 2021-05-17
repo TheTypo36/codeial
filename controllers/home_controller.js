faeab38c0955ae6d4f898dcd92e0a8dfef454c26
Ashish Raghuvanshi <thetypo36@gmail.com>
2021-05-17 18:56:54 +0530

    authenticate the user before allowing him to post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,13 +1,17 @@
+const { localsName } = require('ejs');
 const Post = require('../models/post');
 module.exports.home = function (req, res) {
     // res.end("<h1>express is up for codeial</h1>");
     // console.log(req);
     // console.log('hello');
-    Post.find({}, function (err, posts) {
-        res.render('home', {
-            title: "Codeial | Home",
-            posts: posts
-        });
-    })
+
+    Post.find({}).populate('user').exec(
+        function (err, posts) {
+            res.render('home', {
+                title: "Codeial | Home",
+                posts: posts
+            });
+        }
+    );
 
 }
