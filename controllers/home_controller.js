1486f6d33a3c692d0c0413e8b04dea1e0be8ef72
Ashish Raghuvanshi <thetypo36@gmail.com>
2021-05-20 13:00:48 +0530

    show comment and its author along with the post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -5,13 +5,20 @@ module.exports.home = function (req, res) {
     // console.log(req);
     // console.log('hello');
 
-    Post.find({}).populate('user').exec(
-        function (err, posts) {
+    Post.find({})
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        })
+        .exec(function (err, posts) {
             res.render('home', {
                 title: "Codeial | Home",
                 posts: posts
             });
         }
-    );
+        );
 
 }
