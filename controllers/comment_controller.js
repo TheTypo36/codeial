38f72a45f901360557aa31e5eacb380aefb5f041
Ashish Raghuvanshi <thetypo36@gmail.com>
2021-05-22 13:18:05 +0530

    add comment delete functionality for authorized user

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -1,5 +1,6 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
+const { post } = require('../routes/posts');
 
 module.exports.create = function (req, res) {
     Post.findById(req.body.post, function (err, post) {
@@ -18,5 +19,31 @@ module.exports.create = function (req, res) {
                 res.redirect('/');
             });
         }
+    });
+}
+
+module.exports.destroy = function (req, res) {
+    Comment.findById(req.params.id, function (err, comment) {
+
+        if (comment.user == req.user.id || (
+            Post.findById(comment.post, function (err, post) {
+                if (post.user == req.user.id) {
+                    return true;
+                } else {
+                    return false;
+                }
+            })
+        )) {
+            let postId = comment.post;
+            comment.remove();
+
+            Post.findByIdAndUpdate(postId, { $pull: { comments: req.params.id } }, function (err, post) {
+
+                return res.redirect('back');
+            });
+        } else {
+            return res.redirect('back');
+        }
+
     });
 }
