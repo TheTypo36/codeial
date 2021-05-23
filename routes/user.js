e22d1347eb2e1726cb699391eab71064810efb2d
Ashish Raghuvanshi <thetypo36@gmail.com>
2021-05-23 21:11:30 +0530

    add functionality for updating the user

--- routes/user.js
+++ routes/user.js
@@ -7,7 +7,7 @@ const passport = require('passport');
 const userController = require('../controllers/users_controller');
 
 router.get("/profile/:id", passport.checkAuthentication, userController.profile);
-
+router.post('/update/:id', passport.checkAuthentication, userController.update);
 router.get('/sign-up', userController.signUp);
 router.get('/sign-in', userController.signIn);
 router.post('/create', userController.create);
