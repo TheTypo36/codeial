c624bbd7d8d2aa7fb7a8bc3a5b3730a44f4ce986
Ashish Raghuvanshi <thetypo36@gmail.com>
2021-05-25 11:41:16 +0530

    convert home action into async await function

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,29 +1,34 @@
 const { localsName } = require('ejs');
 const Post = require('../models/post');
 const user = require('../models/user');
-module.exports.home = function (req, res) {
+
+//using async awaiting for handling the chaining of request
+module.exports.home = async function (req, res) {
     // res.end("<h1>express is up for codeial</h1>");
     // console.log(req);
     // console.log('hello');
-
-    Post.find({})
-        .populate('user')
-        .populate({
-            path: 'comments',
-            populate: {
-                path: 'user'
-            }
-        })
-        .exec(function (err, posts) {
-            user.find({}, function (err, users) {
-
-                res.render('home', {
-                    title: "Codeial | Home",
-                    posts: posts,
-                    all_users: users
-                });
+    try {
+        let posts = await Post.find({})
+            .populate('user')
+            .populate({
+                path: 'comments',
+                populate: {
+                    path: 'user'
+                }
             });
-        }
-        );
+
+        let users = await user.find({});
+
+        res.render('home', {
+            title: "Codeial | Home",
+            posts: posts,
+            all_users: users
+        });
+
+
+    } catch (err) {
+        console.log('Error', err);
+        return;
+    }
 
 }
